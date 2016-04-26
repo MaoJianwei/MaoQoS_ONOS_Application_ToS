3f793b64c097a34363371e037c747e637cbf9829
MaoJianwei <maojianwei2012@126.com>
2016-04-26 12:06:57 +0800

    default Qos is ready!

--- src/main/java/org/onosproject/mao/qos/tos/impl/MaoQosTos.java
+++ src/main/java/org/onosproject/mao/qos/tos/impl/MaoQosTos.java
@@ -30,21 +30,38 @@ import org.onlab.packet.UDP;
 import org.onosproject.core.Application;
 import org.onosproject.core.ApplicationId;
 import org.onosproject.core.CoreService;
+import org.onosproject.mao.qos.api.impl.qdisc.MaoTbfQdiscObj;
 import org.onosproject.mao.qos.tos.intf.MaoQosTosService;
 import org.onosproject.mao.qos.api.impl.classify.MaoHtbClassObj;
 import org.onosproject.mao.qos.api.impl.qdisc.MaoHtbQdiscObj;
 import org.onosproject.mao.qos.api.intf.MaoQosObj;
 import org.onosproject.mao.qos.intf.MaoQosService;
 import org.onosproject.net.DeviceId;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.device.DeviceService;
 import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.FlowRule;
+import org.onosproject.net.flow.FlowRuleEvent;
+import org.onosproject.net.flow.FlowRuleListener;
 import org.onosproject.net.flow.FlowRuleService;
 import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.flow.criteria.Criteria;
+import org.onosproject.net.flow.criteria.Criterion;
+import org.onosproject.net.flow.criteria.IPCriterion;
+import org.onosproject.net.flow.criteria.TcpPortCriterion;
+import org.onosproject.net.flow.criteria.UdpPortCriterion;
+import org.onosproject.net.flow.instructions.Instruction;
+import org.onosproject.net.flow.instructions.Instructions;
 import org.onosproject.net.packet.PacketContext;
 import org.onosproject.net.packet.PacketPriority;
 import org.onosproject.net.packet.PacketProcessor;
 import org.onosproject.net.packet.PacketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -55,6 +72,9 @@ import org.slf4j.LoggerFactory;
 public class MaoQosTos implements MaoQosTosService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final long DEFAULT_QUEUE_ID = 0x10 - 1;
+    private final long FTP_QUEUE_ID = 0x20 - 1;
+    private final long STREAM_QUEUE_ID = 0x30 - 1;
 
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
@@ -69,8 +89,12 @@ public class MaoQosTos implements MaoQosTosService {
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected FlowRuleService flowRuleService;
 
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected DeviceService deviceService;
+
 
     private InnerPacketProcessor innerPacketProcessor;
+    private InnerFlowRuleListener innerFlowRuleListener;
     private ApplicationId appId;
 
 
@@ -81,7 +105,12 @@ public class MaoQosTos implements MaoQosTosService {
 
         appId = coreService.registerApplication("onos.app.mao.qos.tos");
 
-        requestPacket();
+        buildDefaultQos();
+
+        innerFlowRuleListener = new InnerFlowRuleListener();
+        flowRuleService.addListener(innerFlowRuleListener);
+
+//        requestPacket();
 
         log.info("Started");
     }
@@ -90,8 +119,10 @@ public class MaoQosTos implements MaoQosTosService {
     protected void deactivate() {
         log.info("destroy...");
 
-        withdrawPacket();
+//        withdrawPacket();
 
+        flowRuleService.removeListener(innerFlowRuleListener);
+        innerFlowRuleListener = null;
         flowRuleService.removeFlowRulesById(appId);
 
         log.info("Stopped");
@@ -162,6 +193,147 @@ public class MaoQosTos implements MaoQosTosService {
     }
 
 
+    private void buildDefaultQos() {
+        buildOneDefaultQos(DeviceId.deviceId("of:0001111111111111"), 1);
+        buildOneDefaultQos(DeviceId.deviceId("of:0001111111111111"), 3);
+        buildOneDefaultQos(DeviceId.deviceId("of:0002222222222222"), 1);
+        buildOneDefaultQos(DeviceId.deviceId("of:0002222222222222"), 2);
+        buildOneDefaultQos(DeviceId.deviceId("of:0002222222222222"), 3);
+        buildOneDefaultQos(DeviceId.deviceId("of:0002222222222222"), 4);
+    }
+
+    private void buildOneDefaultQos(DeviceId deviceId, int deviceIntfNumber) {
+
+        MaoHtbQdiscObj rootHtb = MaoHtbQdiscObj.builder()
+                .add()
+                .setParent(MaoQosObj.ROOT)
+                .setHandleOrClassId("1:")
+                .setDeviceId(deviceId)
+                .setDeviceIntfNumber(deviceIntfNumber)
+                .build();
+        maoQosService.Apply(rootHtb);
+
+        MaoHtbClassObj parentHtbClass = MaoHtbClassObj.builder()
+                .add()
+                .setParent(rootHtb)
+                .setHandleOrClassId("1:5")
+                .setDeviceId(deviceId)
+                .setDeviceIntfNumber(deviceIntfNumber)
+                .rate(10, MaoQosObj.RATE_MBIT)
+                .ceil(10, MaoQosObj.RATE_MBIT)
+                .burst(10, MaoQosObj.SIZE_MBIT)
+                .cburst(10, MaoQosObj.SIZE_MBIT)
+                .build();
+        maoQosService.Apply(parentHtbClass);
+
+        MaoHtbClassObj leafHtbClass = MaoHtbClassObj.builder()
+                .add()
+                .setParent(parentHtbClass)
+                .setHandleOrClassId("1:10")
+                .setDeviceId(deviceId)
+                .setDeviceIntfNumber(deviceIntfNumber)
+                .rate(1, MaoQosObj.RATE_KBIT)
+                .ceil(10, MaoQosObj.RATE_MBIT)
+                .burst(10, MaoQosObj.SIZE_MBIT)
+                .cburst(10, MaoQosObj.SIZE_MBIT)
+                .build();
+        maoQosService.Apply(leafHtbClass);
+
+        MaoTbfQdiscObj leafTbf = MaoTbfQdiscObj.builder()
+                .add()
+                .setParent(leafHtbClass)
+                .setHandleOrClassId("10:")
+                .setDeviceId(deviceId)
+                .setDeviceIntfNumber(deviceIntfNumber)
+                .rate(10, MaoQosObj.RATE_MBIT)
+                .burst(10, MaoQosObj.SIZE_MBIT)
+                .limit(5, MaoQosObj.SIZE_MBYTE)
+                .build();
+        maoQosService.Apply(leafTbf);
+    }
+
+
+    private class InnerFlowRuleListener implements FlowRuleListener {
+
+        @Override
+        public void event(FlowRuleEvent event) {
+
+            if (event.type().equals(FlowRuleEvent.Type.RULE_ADD_REQUESTED)) {
+
+                FlowRule flowRule = event.subject();
+
+
+                List queueInst = flowRule.treatment().allInstructions().stream()
+                        .filter(inst -> inst.type().equals(Instruction.Type.QUEUE))
+                        .collect(Collectors.toList());
+
+                if (!queueInst.isEmpty()) {
+                    return;
+                }
+
+
+                TrafficSelector trafficSelector = flowRule.selector();
+
+                Criterion ipSrc = trafficSelector.getCriterion(Criterion.Type.IPV4_SRC);
+                Criterion ipDst = trafficSelector.getCriterion(Criterion.Type.IPV4_DST);
+
+                if (ipSrc == null && ipDst == null) {
+                    return;
+                }
+
+
+                if (ipSrc != null && ((IPCriterion) ipSrc).ip().equals(IpPrefix.valueOf("10.0.0.1/32"))) {
+
+
+                } else if (ipDst != null && ((IPCriterion) ipDst).ip().equals(IpPrefix.valueOf("10.0.0.1/32"))) {
+
+                    Criterion tcpSrc = trafficSelector.getCriterion(Criterion.Type.TCP_SRC);
+                    Criterion udpSrc = trafficSelector.getCriterion(Criterion.Type.UDP_SRC); // TODO - CHECK - IS THIS PORT?
+
+                    if (tcpSrc != null && ((TcpPortCriterion) tcpSrc).tcpPort().toInt() == 22) {
+
+                        // TODO - Create Qos
+                        // TODO - CREATE SET_QUEUE
+                    } else if (udpSrc != null && ((UdpPortCriterion) udpSrc).udpPort().toInt() == 1355) {
+
+                        // TODO - Create Qos
+                        // TODO - CREATE SET_QUEUE
+                    }
+
+                    return;
+                }
+
+                setDefaultQueue(flowRule);
+
+            }
+        }
+
+        private void setDefaultQueue(FlowRule flowRule) {
+
+
+            List<Instruction> outputInst = flowRule.treatment().allInstructions().stream()
+                    .filter(inst -> inst.type().equals(Instruction.Type.OUTPUT))
+                    .collect(Collectors.toList());
+            if (outputInst.size() > 1) {
+                log.warn("OUTPUT is more than 1 !!!");
+            } else if (outputInst.isEmpty()) {
+                log.warn("OUTPUT is not exist !!!");
+            } else {
+
+                PortNumber port = ((Instructions.OutputInstruction) outputInst.get(0)).port();
+                long queueId = lookupDefaultQueueId(flowRule.deviceId(), port.toLong());
+
+                flowRule.treatment().immediate().add(0, Instructions.setQueue(queueId, port));
+
+                flowRuleService.applyFlowRules(flowRule);
+            }
+        }
+
+        private long lookupDefaultQueueId(DeviceId deviceId, long deviceIntfNumber) {
+            return DEFAULT_QUEUE_ID;
+        }
+    }
+
     private class InnerPacketProcessor implements PacketProcessor {
 
         @Override
@@ -189,13 +361,13 @@ public class MaoQosTos implements MaoQosTosService {
                 }
 
                 TCP tcpPkt = (TCP) ipPkt.getPayload();
-                
-                if(tcpPkt.getDestinationPort() == 80){
+
+                if (tcpPkt.getDestinationPort() == 80) {
 
                     // TODO - config QOS
                     // todo - push flow entries guide traffic
 
-                } else if (tcpPkt.getDestinationPort() == 22){
+                } else if (tcpPkt.getDestinationPort() == 22) {
 
                 } else {
 
